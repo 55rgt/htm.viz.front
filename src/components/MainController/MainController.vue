accadcf1c30a0a293b3be9c7c352b13676a40f32
Gong Hyeonsik <hyeonsik.gong@gmail.com>
2020-06-09 12:22:03 +0900

    update

--- src/components/MainController/MainController.vue
+++ src/components/MainController/MainController.vue
@@ -1,45 +1,45 @@
 <template>
-  <div class="controller-container">
-    <div class="controller-grid ma-3-right">
-      <div class="controller-header"> Filter </div>
-      <div class="controller-body"></div>
-    </div>
-    <div class="controller-empty"></div>
-    <div class="controller-grid">
-      <div class="controller-header pa-4-left"> Sort </div>
-      <div class="controller-body">
-        <div class="controller-sort-list">
-          <div class="controller-sort">
-            <div class="sort-index"> 1. </div>
-            <div class="controller-dropdown">
-              <v-select class="drop-down"
-                :items="this.$store.state.totalMetrics"
-                label=" "
-                dense
-                height="28px"
-              ></v-select>
-            </div>
-            <div class="controller-buttons">
-              <div class="controller-button"> asc </div>
-              <div class="controller-button"> desc </div>
-            </div>
-            <div class="controller-delete">
-              <font-awesome-icon class="delete-icon" :icon="['fas', 'trash']"/>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="controller-empty"></div>
-    <div class="controller-grid ma-3-left">
-      <div class="controller-header pa-4-left"> Node Configuration </div>
-      <div class="controller-body">
-        <div class="date-unit-container"> Date Unit </div>
-        <div class="metrics-container">
-        </div>
-      </div>
-    </div>
-  </div>
+<!--  <div class="controller-container">-->
+<!--    <div class="controller-grid ma-3-right">-->
+<!--      <div class="controller-header"> Filter </div>-->
+<!--      <div class="controller-body"></div>-->
+<!--    </div>-->
+<!--    <div class="controller-empty"></div>-->
+<!--    <div class="controller-grid">-->
+<!--      <div class="controller-header pa-4-left"> Sort </div>-->
+<!--      <div class="controller-body">-->
+<!--        <div class="controller-sort-list">-->
+<!--          <div class="controller-sort">-->
+<!--            <div class="sort-index"> 1. </div>-->
+<!--            <div class="controller-dropdown">-->
+<!--              <v-select class="drop-down"-->
+<!--                :items="this.$store.state.totalMetrics"-->
+<!--                label=" "-->
+<!--                dense-->
+<!--                height="28px"-->
+<!--              ></v-select>-->
+<!--            </div>-->
+<!--            <div class="controller-buttons">-->
+<!--              <div class="controller-button"> asc </div>-->
+<!--              <div class="controller-button"> desc </div>-->
+<!--            </div>-->
+<!--            <div class="controller-delete">-->
+<!--              <font-awesome-icon class="delete-icon" :icon="['fas', 'trash']"/>-->
+<!--            </div>-->
+<!--          </div>-->
+<!--        </div>-->
+<!--      </div>-->
+<!--    </div>-->
+<!--    <div class="controller-empty"></div>-->
+<!--    <div class="controller-grid ma-3-left">-->
+<!--      <div class="controller-header pa-4-left"> Node Configuration </div>-->
+<!--      <div class="controller-body">-->
+<!--        <div class="date-unit-container"> Date Unit </div>-->
+<!--        <div class="metrics-container">-->
+<!--        </div>-->
+<!--      </div>-->
+<!--    </div>-->
+<!--  </div>-->
 </template>
 
 <script src='./MainController.ts'>
